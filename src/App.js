583225000a057281eea887a41e91246dc2a602f5
muzahid37 <muzahidul372709@gmail.com>
2022-06-09 13:19:39 +0600

    add contuct us

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Pages/Home/Home';
 import Footer from './Components/Sheared/Footer';
+import Contact from './Components/Pages/Contact/Contact';
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <Contact></Contact>
       <Footer></Footer>
      
     </div>
